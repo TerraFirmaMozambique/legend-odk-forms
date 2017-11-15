4d795a8843f15ae1819075394bf85a498a2e02dc
TSIG <terry@terrafirma.co.mz>
2017-11-15 12:53:09 +0200

    updated

--- SQL/part_form_l_pontas.sql
+++ SQL/part_form_l_pontas.sql
@@ -1,45 +1,47 @@
 -- Legend update points in L
 
 
-CREATE TABLE public.update_form_l_pontos
+CREATE TABLE public.update_form_l_registrar_pontos
 (
-  geom geometry(Point,0),
-  latitude double precision,
-  longitude double precision,
-  altitude double precision,
-  accuracy double precision,
-  limits character varying,
-  parent_key character varying,
-  key character varying NOT NULL
+    geom geometry(Point, 0),
+    latitude double precision,
+    longitude double precision,
+    altitude double precision,
+    accuracy double precision,
+    upngpslimit character varying COLLATE pg_catalog."default",
+    parentuid character varying COLLATE pg_catalog."default" NOT NULL,
+    key character varying COLLATE pg_catalog."default"
 )
 WITH (
   OIDS=TRUE
 );
-ALTER TABLE public.update_form_l_pontos
+ALTER TABLE public.update_form_l_registrar_pontos
   OWNER TO postgres;
-GRANT ALL ON TABLE public.update_form_l_pontos TO postgres;
 
-COPY public.update_form_l_pontos FROM '/var/lib/share/projects/legend/dbupdate/legend_L_pontos.csv'  USING DELIMITERS ',' WITH NULL AS '' CSV HEADER;
-	
-DELETE FROM public.update_form_l_pontos a
+GRANT ALL ON TABLE public.update_form_l_registrar_pontos TO postgres;
+GRANT ALL ON TABLE public.update_form_l_registrar_pontos TO public;
+
+	COPY public.update_form_l_registrar_pontos FROM '/var/lib/share/projects/legend/dbupdate/form_l_registrar_pontos.csv'  USING DELIMITERS ',' WITH NULL AS '' CSV HEADER ENCODING 'latin1';
+
+DELETE FROM public.update_form_l_registrar_pontos a
 WHERE a.ctid <> (SELECT min(b.ctid)
-                 FROM   public.update_form_l_pontos b
+                 FROM   public.update_form_l_registrar_pontos b
                  WHERE  a.key = b.key);
 	
-	DELETE FROM public.update_form_l_pontos
-WHERE EXISTS (SELECT 1 FROM public.form_l_pontos 
-WHERE key = public.update_form_l_pontos.key );
+	DELETE FROM public.update_form_l_registrar_pontos
+WHERE EXISTS (SELECT 1 FROM public.form_l_registrar_pontos 
+WHERE key = public.update_form_l_registrar_pontos.key );
 
-SELECT UpdateGeometrySRID('form_l_pontos','geom',0);
+SELECT UpdateGeometrySRID('form_l_registrar_pontos','geom',0);
 
-INSERT INTO public.form_l_pontos(
-            geom, latitude, longitude, altitude, accuracy, limits, parent_key, 
+INSERT INTO public.form_l_registrar_pontos(
+            geom, latitude, longitude, altitude, accuracy, upngpslimit, parentuid, 
             key)
-SELECT geom, latitude, longitude, altitude, accuracy, limits, parent_key, 
+SELECT geom, latitude, longitude, altitude, accuracy, upngpslimit, parentuid, 
        key
-  FROM public.update_form_l_pontos;
-  
-SELECT UpdateGeometrySRID('form_l_pontos','geom',4326);
+  FROM public.update_form_l_registrar_pontos;
 
 
-DROP TABLE public.form_l_pontos;
+SELECT UpdateGeometrySRID('form_l_registrar_pontos','geom',4326);
+
+DROP TABLE public.update_form_l_registrar_pontos;
